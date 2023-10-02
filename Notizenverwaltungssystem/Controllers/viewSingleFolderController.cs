d875666fe1a920a4dc5b852da81d3adfe5422ff9
nilsNdev <nils.naepflin@gmail.com>
2023-10-02 08:28:55 +0200

    Neue Änderungen

--- Notizenverwaltungssystem/Controllers/viewSingleFolderController.cs
+++ Notizenverwaltungssystem/Controllers/viewSingleFolderController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Notizenverwaltungssystem.otherClasses;
+using Notizenverwaltungssystem.Repositories;
 
 namespace Notizenverwaltungssystem.Controllers{
     [Route("[controller]")]
@@ -20,6 +21,32 @@ namespace Notizenverwaltungssystem.Controllers{
                 }
             }
             catch (Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+            return Content(html, "text/html");
+        }
+        [HttpGet("GetByID")]
+        public IActionResult GetByID(int id){
+            string html = "";
+            connection_class conn;
+            try
+            {
+                if (Settings.ActiveUserName == "" || Settings.ActiveUserName == null)
+                {
+                    html = System.IO.File.ReadAllText("web/login");
+                }
+                else
+                {
+                    conn = connection_class.getInstance();
+                    string query = $"SELECT folder_name FROM folder WHERE ID ={id}";
+                    string folderName = conn.executeWithReturnValue<string>(query);
+                    html = System.IO.File.ReadAllText("web/viewSingleFolder.html");
+                    html = html.Replace("@id", id.ToString());
+                    html = html.Replace("@folderName", folderName);
+                }
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine(ex.Message);
             }
             return Content(html, "text/html");
